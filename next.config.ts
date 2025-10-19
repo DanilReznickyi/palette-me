7576ed845b0da2074d4b05db13cec2e90f33110d
DanilReznickyi <danilrez.it@gmail.com>
2025-10-19 16:23:03 +0200

    ci: disable API for static export and deploy to Pages

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
 // next.config.ts
 const isProd = process.env.NODE_ENV === "production";
 
-const repo = "palette-me";
-
 export default {
   output: "export",
   images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === "production" ? "/palette-me" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/palette-me/" : "",
+  basePath: isProd ? "/palette-me" : "",
+  assetPrefix: isProd ? "/palette-me/" : "",
   trailingSlash: true,
+
+
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 } as const;
-
